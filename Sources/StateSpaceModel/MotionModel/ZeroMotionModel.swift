f4acf5170abe82f0c250acfd49d18dcf95f4733f
Vincent Esche <regexident@gmail.com>
2019-10-16 20:22:28 +0200

    Made `ZeroMotionModel` dimension-agnostic

--- Sources/StateSpaceModel/MotionModel/ZeroMotionModel.swift
+++ Sources/StateSpaceModel/MotionModel/ZeroMotionModel.swift
@@ -3,18 +3,8 @@ import Foundation
 import Surge
 
 public class ZeroMotionModel {
-    private let nominalDimensions: StateDimensions
-
-    public init(
-        dimensions: StateDimensionsProtocol
-    ) {
-        self.nominalDimensions = StateDimensions(state: dimensions.state)
-    }
-}
-
-extension ZeroMotionModel: DimensionalModelProtocol {
-    public var dimensions: DimensionsProtocol {
-        return self.nominalDimensions
+    public init() {
+        // nothing
     }
 }
 
@@ -30,18 +20,6 @@ extension ZeroMotionModel: MotionModelProtocol {
 
 extension ZeroMotionModel: DimensionsValidatable {
     public func validate(for dimensions: DimensionsProtocol) throws {
-        typealias TypedDimensions = StateDimensionsProtocol
-
-        guard let typedDimensions = dimensions as? TypedDimensions else {
-            throw DimensionsError.invalidType(
-                message: "Type \(type(of: dimensions)) does not conform to \(TypedDimensions.self)"
-            )
-        }
-
-        guard self.nominalDimensions.state == typedDimensions.state else {
-            throw DimensionsError.invalidValue(
-                message: "Expected dimensions of \(typedDimensions.state) in `self.dimensions`, found \(self.nominalDimensions.state)"
-            )
-        }
+        // This model cannot fail.
     }
 }
