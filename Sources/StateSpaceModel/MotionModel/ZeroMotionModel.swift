6cccd73db7615771ec342c76781afbc4a3e8e68d
Vincent Esche <regexident@gmail.com>
2019-10-17 15:59:47 +0200

    Made `ZeroMotionModel` conform to `DifferentiableMotionModelProtocol`

--- Sources/StateSpaceModel/MotionModel/ZeroMotionModel.swift
+++ Sources/StateSpaceModel/MotionModel/ZeroMotionModel.swift
@@ -4,8 +4,21 @@ import Surge
 import StateSpace
 
 public class ZeroMotionModel {
+    private var cachedIdentity: Matrix<Double>? = nil
+
     public init() {
         // nothing
+    }
+
+    private func identity(size: Int) -> Matrix<Double> {
+        let identity = self.cachedIdentity ?? Matrix.identity(size: size)
+
+        assert(identity.rows == size, "Malformed identity matrix")
+        assert(identity.columns == size, "Malformed identity matrix")
+
+        self.cachedIdentity = identity
+
+        return identity
     }
 }
 
@@ -16,6 +29,16 @@ extension ZeroMotionModel: Statable {
 extension ZeroMotionModel: UncontrollableMotionModelProtocol {
    public func apply(state x: State) -> State {
         return x
+    }
+}
+
+extension ZeroMotionModel: Differentiable {
+    public typealias Jacobian = Matrix<Double>
+}
+
+extension ZeroMotionModel: DifferentiableMotionModelProtocol {
+    public func jacobian(state x: State) -> Jacobian {
+        return self.identity(size: x.dimensions)
     }
 }
 
