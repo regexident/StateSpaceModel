98d4adc42bd1643de47132a82d0cb3eb35f1319c
Vincent Esche <regexident@gmail.com>
2019-10-21 19:38:30 +0200

    Removed `struct Dimensions`/`protocol DimensionsProtocol` & `protocol DimensionsValidatable`

--- Sources/StateSpaceModel/ModelProtocols.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol DimensionsValidatable {
-    /// Validate the model for a given dimensional environment, or throw `Error`.
-    ///
-    /// - Parameters:
-    ///   - dimensions: the environment's dimensions
-    func validate(for dimensions: DimensionsProtocol) throws
-}
